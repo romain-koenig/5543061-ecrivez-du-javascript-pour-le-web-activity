fbb72f08e15e2b0462522912bde301d6f9c5b75e
Romain KOENIG <romain.koenig@gmail.com>
2019-04-26 17:59:12 +0200

    Amélioration fonctionnement avec Skate Lorem Ipsum

--- src/blog.js
+++ src/blog.js
@@ -2,13 +2,15 @@ const api = "https://mashape-community-skate-ipsum.p.rapidapi.com/1/0/JSON";
 
 const loadButton = document.getElementById('load-button');
 
-function getRequest(url) {
+function getRequest(url, isTitle) {
   return new Promise((resolve, reject) => {
     let request = new XMLHttpRequest();
     request.open('GET', url);
 
     request.setRequestHeader("X-RapidAPI-Host", "mashape-community-skate-ipsum.p.rapidapi.com");
     request.setRequestHeader("X-RapidAPI-Key", "b7bfae06bcmshf712d6c0796cac4p13cb8ejsnb70722f5b848");
+    request.setRequestHeader("start", "0");
+    request.setRequestHeader("paragraphs", "1");
 
     request.onreadystatechange = () => {
       if (request.readyState === 4) {
@@ -17,7 +19,7 @@ function getRequest(url) {
           reject(JSON.parse(request.response));
         }
         console.log("OK - " + request.response);
-        resolve(JSON.parse(request.response));
+        resolve(isTitle ? JSON.parse(request.response)[0].substring(0, 50) : JSON.parse(request.response));
       }
     };
     request.send();
@@ -26,8 +28,8 @@ function getRequest(url) {
 
 async function getBlogPost() {
   //Quick hack pour changer le provider de Lorem Ipsum (celui fourni ne fonctionne pas)
-  const titlePromise = getRequest(api);
-  const loremPromise = getRequest(api);
+  const titlePromise = getRequest(api, true);
+  const loremPromise = getRequest(api, false);
   try {
     let [titleResponse, loremResponse] = await Promise.all([titlePromise, loremPromise]);
     document.querySelector('main').appendChild(buildPostElement(titleResponse, loremResponse));
