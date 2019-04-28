53995c6087b35a8d8717ce77fa39d9727969cad0
Romain KOENIG <romain.koenig@gmail.com>
2019-04-29 00:01:36 +0200

    Remplacement du webservice d'origine
    Le ebservice Lorem Ipsum fourni par défut dans les sources ne fonctionne
    pas (vu par MP avec auteur du cours)
    Replacé par "Skate Ipsum" en dirty hack pour avoir un code fonctionnel

--- Src/blog.js
+++ Src/blog.js
@@ -1,16 +1,25 @@
-const api = 'https://master-7rqtwti-fus3tltfddn7w.eu-2.platformsh.site';
+const api = "https://mashape-community-skate-ipsum.p.rapidapi.com/1/0/JSON";
+
 const loadButton = document.getElementById('load-button');
 
-function getRequest(url) {
+function getRequest(url, isTitle) {
   return new Promise((resolve, reject) => {
     let request = new XMLHttpRequest();
     request.open('GET', url);
+
+    request.setRequestHeader("X-RapidAPI-Host", "mashape-community-skate-ipsum.p.rapidapi.com");
+    request.setRequestHeader("X-RapidAPI-Key", "b7bfae06bcmshf712d6c0796cac4p13cb8ejsnb70722f5b848");
+    request.setRequestHeader("start", "0");
+    request.setRequestHeader("paragraphs", "1");
+
     request.onreadystatechange = () => {
       if (request.readyState === 4) {
         if (request.status !== 200) {
+          console.log("ERREUR - " + request.response);
           reject(JSON.parse(request.response));
         }
-        resolve(JSON.parse(request.response));
+        console.log("OK - " + request.response);
+        resolve(isTitle ? JSON.parse(request.response)[0].substring(0, 50) : JSON.parse(request.response));
       }
     };
     request.send();
@@ -18,11 +27,12 @@ function getRequest(url) {
 }
 
 async function getBlogPost() {
-  const titlePromise = getRequest(api + '/generate-title');
-  const loremPromise = getRequest(api + '/generate-lorem');
+  //Quick hack pour changer le provider de Lorem Ipsum (celui fourni ne fonctionne pas)
+  const titlePromise = getRequest(api, true);
+  const loremPromise = getRequest(api, false);
   try {
     let [titleResponse, loremResponse] = await Promise.all([titlePromise, loremPromise]);
-    document.querySelector('main').appendChild(buildPostElement(titleResponse.title, loremResponse.lorem));
+    document.querySelector('main').appendChild(buildPostElement(titleResponse, loremResponse));
   } catch (error) {
     document.querySelector('main').appendChild(buildPostElement('Une erreur est survenue !', error));
   }
