21009068cb502f85b5b490ee53308b911f5bf1f3
Romain KOENIG <romain.koenig@gmail.com>
2019-04-29 00:25:57 +0200

    Création du fichier de configuration webpack
    Paramétrage minimal et modification des sources .js dans les .html pour
    pointer vers les fichiers bundle générés par webpack.
    Fonctionnement OK.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    //mode: "production",
+    mode: "development",
+    entry: {
+        fcn: './Src/functions.js',
+        blog: './Src/blog.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'Dist')
+    }
+};
